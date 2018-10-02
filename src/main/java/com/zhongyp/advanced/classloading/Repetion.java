b2833e8a16bc85860e46f6d8af93961119a29787
zhongyp <zhongyp001@163.com>
2018-10-02 09:16:23 +0800

    update user

--- src/main/java/com/zhongyp/advanced/classloading/Repetion.java
+++ src/main/java/com/zhongyp/advanced/classloading/Repetion.java
@@ -1,7 +1,7 @@
 package com.zhongyp.advanced.classloading;
 import java.util.*;
 /**
- * Created by camda on 2017/6/29.
+ * Created by zhongyp on 2017/6/29.
  * 外部类调用getInstance方法，系统加载Example
  * 类加载完成后，会初始化静态变量
  * 初始化Example时，调用构造函数，构造函数内调用静态变量test，而此时静态变量未被赋值，因此会报错ExceptionInInitializerError
